194486f3d5e7b58802ab1342381b5a430e30e5f1
Sander Boer <sanderboer@mauc.nl>
2020-05-25 17:58:49 +0200

    Too lazy to comment

--- keyboards/absolem_mauc/keymaps/default/keymap.c
+++ keyboards/absolem_mauc/keymaps/default/keymap.c
@@ -50,20 +50,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
   [_COLEMAK] = LAYOUT_absolem(
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_NO,
-    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,   
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
     KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    SYM,     MISC,    QWERTY,
     KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), LT(NAV, KC_SPC),  MT(MOD_MEH, KC_ENTER),  KC_LSHIFT,  LT(NUM, KC_BSPACE),  MT(MOD_LGUI, KC_ESCAPE), KC_NO, KC_NO
 ),
    [_QWERTY] = LAYOUT_absolem(
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_NO,   
+       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_NO,
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    SYM,     MISC,    COLEMAK,
        KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), LT(NAV, KC_SPC),  MT(MOD_MEH, KC_ENTER),  KC_LSHIFT,  LT(NUM, KC_BSPACE),  MT(MOD_LGUI, KC_ESCAPE), KC_NO, KC_NO
 ),
 
    [_SYM] = LAYOUT_absolem(
        KC_RABK, KC_RCBR, KC_RBRC, KC_RPRN, KC_BSLASH,  KC_TILD, KC_CIRC, KC_NO, KC_NO,    KC_AT ,
-       KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, KC_SLASH,  KC_PERC, KC_ASTR, KC_NO, KC_COLN,  KC_SCLN,   
+       KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, KC_SLASH,  KC_PERC, KC_ASTR, KC_NO, KC_COLN,  KC_SCLN,
     KC_HASH, KC_DLR,  KC_QUES, KC_EXLM, KC_PIPE, KC_AMPR, KC_EQL,  KC_NO, KC_PLUS, KC_QUOT,
     KC_NO,   KC_NO,   KC_UNDS, KC_QUOT,  KC_DQT,  KC_DOT,  KC_COMM, KC_PMNS, KC_NO, KC_NO
 ),
@@ -71,20 +71,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
        LCA(KC_M), LCA(KC_LEFT), LCA(KC_SPC), LCA(KC_RIGHT), LCA(KC_UP), KC_PGUP, KC_HOME, KC_SPC,  KC_END, LCTL(KC_GRAVE),
        // KC_MUTE,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_VOLU,    KC_PGUP,   KC_HOME, KC_SPC,  KC_END, LCTL(KC_GRAVE),
-       KC_RGUI,    KC_RALT,    KC_RSFT,    KC_RCTRL,   KC_VOLD,    KC_PGDOWN, KC_NO,   KC_UP,   KC_NO,    KC_TAB,   
+       OSM(MOD_LGUI|MOD_LSFT), OSM(MOD_LALT), OSM(MOD_LALT|MOD_LCTL), OSM(MOD_HYPR), LCA(KC_DOWN),    KC_PGDOWN, KC_NO,   KC_UP,   KC_NO,    KC_TAB,
        LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_D), LCTL(KC_V), KC_APP,    KC_LEFT,  KC_DOWN, KC_RIGHT, KC_ENTER,
-       KC_NO,      KC_NO,      KC_NO,      KC_X,     KC_NO,      KC_DEL,    KC_BSPACE, KC_ESCAPE, KC_NO, KC_NO
+       KC_NO,      KC_NO, OSM(MOD_LCTL), NAV, OSM(MOD_LALT),      KC_DEL,    KC_BSPACE, KC_ESCAPE, KC_NO, KC_NO
 ),
   [_NUM] = LAYOUT_absolem(
       KC_F1,  KC_F2,  KC_F3,  KC_F4,   KC_F5,      KC_PMNS, KC_P7, KC_P8, KC_P9, KC_BSPACE,
       KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,     KC_P0,   KC_P4, KC_P5, KC_6,  KC_PDOT,
-      KC_F11, KC_F12, KC_F13, KC_PSCR, KC_PAUS,   KC_PPLS, KC_P1, KC_P2, KC_P3, KC_PCMM, 
+      KC_F11, KC_F12, KC_F13, KC_PSCR, KC_PAUS,   KC_PPLS, KC_P1, KC_P2, KC_P3, KC_PCMM,
       KC_NO,  KC_NO,  KC_TAB, KC_SPC,  KC_ENTER,   KC_NO,   KC_BSPACE, KC_NO, KC_NO, KC_NO
                           ),
   [_MISC] = LAYOUT_absolem(
-      LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9), LGUI(KC_0), 
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+      LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9), LGUI(KC_0),
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
                            )
 };
