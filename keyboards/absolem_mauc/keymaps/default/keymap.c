974ddb37f136b3285c77226517d625dc2f5b6a36
Sander Boer <sanderboer@mauc.nl>
2020-06-08 11:52:38 +0200

    new nav and num reshuffle

--- keyboards/absolem_mauc/keymaps/default/keymap.c
+++ keyboards/absolem_mauc/keymaps/default/keymap.c
@@ -93,14 +93,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_ESCAPE,
       KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
       KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,     KC_H,   SYM,     MISC,    QWERTY,
-      KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), MT(MOD_LGUI, KC_SPC), MT(MOD_LALT, KC_ESCAPE),  MT(MOD_LSHIFT, KC_ENTER),  LT(NAV, KC_BSPACE),  LT(NUM, KC_ESCAPE), KC_NO, KC_NO
+      KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), MT(MOD_LGUI, KC_SPC), MT(MOD_LALT, KC_ESCAPE),  MT(KC_LSHIFT, KC_ENTER),  LT(NAV, KC_BSPACE),  LT(NUM, KC_ESCAPE), KC_NO, KC_NO
                               ),
 
   [_QWERTY] = LAYOUT_absolem(
       KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
       KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_NO,
       KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, SYM,  MISC, COLEMAK,
-      KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), MT(MOD_LGUI, KC_SPC), MT(MOD_LALT, KC_ESCAPE),  MT(MOD_LSHIFT, KC_ENTER),  LT(NAV, KC_BSPACE),  LT(NUM, KC_ESCAPE), KC_NO, KC_NO                             ),
+      KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), MT(MOD_LGUI, KC_SPC), MT(MOD_LALT, KC_ESCAPE),  MT(KC_LSHIFT, KC_ENTER),  LT(NAV, KC_BSPACE),  LT(NUM, KC_ESCAPE), KC_NO, KC_NO                             ),
 
    [_SYM] = LAYOUT_absolem(
     KC_RABK, KC_RCBR, KC_RBRC, KC_RPRN, KC_BSLASH,  KC_TILD, KC_CIRC, KC_PMNS, KC_PLUS, KC_AT ,
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_PMNS, KC_P7, KC_P8, KC_P9, KC_EQL,  KC_F1,  KC_F2,  KC_F3, KC_F4,   KC_F5,
       KC_PPLS, KC_P4, KC_P5, KC_6,  KC_PDOT, KC_F6,  KC_F7,  KC_F8, KC_F9,   KC_F10,
       KC_P0,   KC_P1, KC_P2, KC_P3, KC_PCMM, KC_F11, KC_F12, KC_NO, KC_PSCR, KC_PAUS,
-      KC_NO,  KC_NO,  KC_TRANS, KC_TRANS,  KC_TRANS,   KC_TRANS,    KC_TRANS, KC_TRANS, KC_NO, KC_NO
+      KC_NO,  KC_NO,  KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_NO, KC_NO
                           ),
 
   [_MISC] = LAYOUT_absolem(
