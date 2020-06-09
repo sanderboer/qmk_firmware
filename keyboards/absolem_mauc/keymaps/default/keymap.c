fa23c02ae5edee5cf6f19dab02dbb4be0f50d91d
Sander Boer <sanderboer@mauc.nl>
2020-06-09 10:09:07 +0200

    Too lazy to comment

--- keyboards/absolem_mauc/keymaps/default/keymap.c
+++ keyboards/absolem_mauc/keymaps/default/keymap.c
@@ -90,17 +90,17 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
   [_COLEMAK] = LAYOUT_absolem(
-      KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_ESCAPE,
+      KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_ALGR,
       KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-      KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,     KC_H,   SYM,     MISC,    QWERTY,
-      KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), MT(MOD_LGUI, KC_SPC), MT(MOD_LALT, KC_ESCAPE),  MT(KC_LSHIFT, KC_ENTER),  LT(NAV, KC_BSPACE),  LT(NUM, KC_ESCAPE), KC_NO, KC_NO
+      KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,     KC_H,   NUM,     MISC,    QWERTY,
+      KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), MT(MOD_LGUI, KC_SPC), MT(MOD_LALT, KC_ESCAPE),  MT(MOD_LSFT, KC_ENTER),  LT(NAV, KC_BSPACE),  LT(SYM, KC_ESCAPE), KC_NO, KC_NO
                               ),
 
   [_QWERTY] = LAYOUT_absolem(
       KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
-      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_NO,
-      KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, SYM,  MISC, COLEMAK,
-      KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), MT(MOD_LGUI, KC_SPC), MT(MOD_LALT, KC_ESCAPE),  MT(KC_LSHIFT, KC_ENTER),  LT(NAV, KC_BSPACE),  LT(NUM, KC_ESCAPE), KC_NO, KC_NO                             ),
+      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ALGR,
+      KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, NUM,  MISC, COLEMAK,
+      KC_NO, KC_NO, MT(MOD_LCTL, KC_TAB), MT(MOD_LGUI, KC_SPC), MT(MOD_LALT, KC_ESCAPE),  MT(MOD_LSFT, KC_ENTER),  LT(NAV, KC_BSPACE),  LT(SYM, KC_ESCAPE), KC_NO, KC_NO                             ),
 
    [_SYM] = LAYOUT_absolem(
     KC_RABK, KC_RCBR, KC_RBRC, KC_RPRN, KC_BSLASH,  KC_TILD, KC_CIRC, KC_PMNS, KC_PLUS, KC_AT ,
@@ -110,12 +110,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            ),
 
   [_NAV] = LAYOUT_absolem(
-      KC_PGUP,   KC_NO,   KC_UP,   KC_NO,         LCTL(KC_GRAVE), KC_PMNS,  KC_P7, KC_P8,     KC_P9, KC_EQL,
-      KC_PGDOWN, KC_LEFT, KC_DOWN, KC_RIGHT,      LGUI(KC_GRAVE), KC_PPLS,  KC_P4, KC_P5,     KC_6,  KC_PDOT,
-      KC_APP,    KC_HOME, KC_SPC,  KC_END,        LCA(KC_GRAVE),  KC_P0,    KC_P1, KC_P2,     KC_P3, KC_PCMM,
-      KC_NO,     KC_NO,   KC_LCTL, OSM(KC_ALGR) , KC_LALT,        KC_DEL,   NAV,   KC_ESCAPE, KC_NO, KC_NO
+      KC_PGUP,   KC_NO,   KC_UP,   KC_NO,    LCTL(KC_GRAVE), KC_PMNS, KC_P7, KC_P8, KC_P9, KC_EQL,
+      KC_PGDOWN, KC_LEFT, KC_DOWN, KC_RIGHT, LGUI(KC_GRAVE), KC_PPLS, KC_P4, KC_P5, KC_P6, KC_PDOT,
+      KC_APP,    KC_HOME, KC_SPC,  KC_END,   LCA(KC_GRAVE),  KC_P0,   KC_P1, KC_P2, KC_P3, KC_PCMM,
+      KC_NO,     KC_NO,   KC_LCTL, KC_LGUI,  KC_LALT,        KC_LSFT, NAV,   KC_ESCAPE, KC_NO, KC_NO
                            ),
-
+   
   [_NUM] = LAYOUT_absolem(
       KC_PMNS, KC_P7, KC_P8, KC_P9, KC_EQL,  KC_F1,  KC_F2,  KC_F3, KC_F4,   KC_F5,
       KC_PPLS, KC_P4, KC_P5, KC_6,  KC_PDOT, KC_F6,  KC_F7,  KC_F8, KC_F9,   KC_F10,
