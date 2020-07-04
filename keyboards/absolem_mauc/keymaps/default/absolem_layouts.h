f7331b4414e15ddec9ffb95f7c492a8732a306af
Sander Boer <sanderboer@mauc.nl>
2020-07-04 16:26:20 +0200

    Too lazy to comment

--- keyboards/absolem_mauc/keymaps/default/absolem_layouts.h
+++ keyboards/absolem_mauc/keymaps/default/absolem_layouts.h
@@ -38,16 +38,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            ),
 
   [_NUM] = LAYOUT_absolem(
-      KC_PMNS, KC_P7, KC_P8, KC_P9, KC_EQL,  KC_F1,  KC_F2,  KC_F3, KC_F4,   KC_F5,
-      KC_PPLS, KC_P4, KC_P5, KC_6,  KC_PDOT, KC_F6,  KC_F7,  KC_F8, KC_F9,   KC_F10,
-      KC_P0,   KC_P1, KC_P2, KC_P3, KC_PCMM, KC_F11, KC_F12, KC_NO, KC_PSCR, KC_PAUS,
+      KC_PMNS, KC_7, KC_8, KC_9, KC_EQL,  KC_F1,  KC_F2,  KC_F3, KC_F4,   KC_F5,
+      KC_PPLS, KC_4, KC_5, KC_6,  KC_PDOT, KC_F6,  KC_F7,  KC_F8, KC_F9,   KC_F10,
+      KC_0,   KC_1, KC_2, KC_3, KC_PCMM, KC_F11, KC_F12, KC_NO, KC_PSCR, KC_PAUS,
       KC_NO,  KC_NO,  KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_NO, KC_NO
                           ),
 
   [_MISC] = LAYOUT_absolem(
      LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0),
      KC_MUTE,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_VOLU,    RGB_HUI,    RGB_SAI,    RGB_VAI,    KC_NO,      KC_NO,
-     RGB_TOG,    RGB_MOD,    RGB_VAI,    RGB_M_B,    KC_VOLD,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+     RGB_TOG,    RGB_MOD,    RGB_VAI,    RGB_M_B,    KC_VOLD,    KC_NLCK,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO
                            )
 
