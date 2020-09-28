51343ad118b626f43908777879616ba3e913c13c
Sander Boer <sanderboer@mauc.nl>
2020-09-28 20:44:15 +0200

    Too lazy to comment

--- keyboards/signum_absolem/keymaps/default/keymap.c
+++ keyboards/signum_absolem/keymaps/default/keymap.c
@@ -26,7 +26,7 @@
   {k00, k01, k02, k03, k04, KC_NO, KC_NO, k05, k06, k07, k08, k09},  \
   {k10, k11, k12, k13, k14, KC_NO, KC_NO, k15, k16, k17, k18, k19},  \
   {k20, k21, k22, k23, k24, KC_NO, KC_NO, k25, k26, k27, k28, k29},  \
-  {KC_NO, KC_NO, KC_NO, k32, k33,  k34, k35, k36, k37, KC_NO, KC_NO, KC_NO } \
+  {KC_NO, KC_NO, KC_NO, KC_NO, k33, k32,k37,k36, KC_NO, KC_NO, KC_NO, KC_NO } \
 }
 
 #include "absolem_layouts.h"
