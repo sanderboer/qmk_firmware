64367e321520f2a43aa8b1eeaa02c2fbc936b199
Sander Boer <sanderboer@mauc.nl>
2020-10-06 13:56:18 +0200

    Too lazy to comment

--- keyboards/absolem_mauc/keymaps/default/keymap.c
+++ keyboards/absolem_mauc/keymaps/default/keymap.c
@@ -30,7 +30,7 @@
     { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09 }, \
     { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19 }, \
     { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29 }, \
-    { KC_NO, KC_NO, k32, k33, k34, k35, k36, k37, KC_NO, KC_NO }  \
+    { KC_NO, KC_NO, k34, k33, k32, k37, k36, k35, KC_NO, KC_NO }  \
 }
 
 // Light LEDs 9 & 10 in cyan when keyboard layer 1 is active
