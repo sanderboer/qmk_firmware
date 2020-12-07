b1d8083569c4d50ccec22e26e6083a2c85535918
Sander Boer <sanderboer@mauc.nl>
2020-12-07 16:59:33 +0100

    Too lazy to comment

--- keyboards/absolem_mauc/keymaps/default/absolem_layouts.h
+++ keyboards/absolem_mauc/keymaps/default/absolem_layouts.h
@@ -137,8 +137,9 @@ const uint16_t PROGMEM w_f_combo[] = {KC_W, KC_F, COMBO_END};
 const uint16_t PROGMEM a_r_combo[] = {KC_A, KC_R, COMBO_END};
 const uint16_t PROGMEM r_s_combo[] = {KC_R, KC_S, COMBO_END};
 const uint16_t PROGMEM s_t_combo[] = {KC_S, KC_T, COMBO_END};
-const uint16_t PROGMEM f_p_combo[] = {KC_F, KC_P, COMBO_END};
 const uint16_t PROGMEM p_b_combo[] = {KC_P, KC_B, COMBO_END};
+const uint16_t PROGMEM f_p_combo[] = {KC_F, KC_P, COMBO_END};
+const uint16_t PROGMEM f_b_combo[] = {KC_F, KC_B, COMBO_END};
 const uint16_t PROGMEM w_p_combo[] = {KC_W, KC_P, COMBO_END};
 const uint16_t PROGMEM z_x_combo[] = {KC_Z, KC_X, COMBO_END};
 const uint16_t PROGMEM x_d_combo[] = {KC_X, KC_D, COMBO_END};
@@ -171,12 +172,16 @@ const uint16_t PROGMEM j_slash_combo[] = {KC_J, KC_SLASH, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
     // left hand combinations.
   COMBO(q_w_combo, KC_ESCAPE),
-  COMBO(x_d_combo, KC_HOME),
-  COMBO(z_x_combo, KC_INSERT),
   COMBO(p_b_combo, KC_TAB),
-  COMBO(c_v_combo, KC_END),
-  COMBO(j_slash_combo, RESET ),
+  COMBO(w_p_combo, KC_LGUI),
+  
+  COMBO(w_f_combo, KC_LCTL),
+  COMBO(f_p_combo, KC_LSFT),
+  COMBO(f_b_combo, KC_LALT),
   /*
+  COMBO(c_v_combo, KC_END),
+  COMBO(z_x_combo, KC_INSERT),
+  COMBO(x_d_combo, KC_HOME),
   COMBO(f_p_combo, KC_UP),
   COMBO(x_c_combo, KC_LEFT),
   COMBO(c_d_combo, KC_DOWN),
@@ -211,18 +216,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,     KC_NO,   KC_LCTL, KC_LGUI,  KC_NO,            KC_NO,   KC_NO, KC_NO,  KC_NO, KC_NO
                            ),
 
+  // [_NUM] = LAYOUT_absolem(
+  //     KC_F1,  KC_F2,  KC_F3, KC_F4,   KC_F5,          KC_PMNS, KC_7,   KC_8, KC_9,  KC_EQL,
+  //     KC_F6,  CT_F7,  SH_F8, AL_F9,   GU_F10,         KC_PPLS, CT_4,   SH_5, AL_6,  GU_PDOT,
+  //     RESET, KC_PSCR, KC_NO, KC_F11,   KC_F12,        KC_0,    KC_1,   KC_2, KC_3,  KC_PCMM,
+  //     KC_NO,   KC_NO,  KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_NO, KC_NO
+  //                         ),
+
   [_NUM] = LAYOUT_absolem(
-      KC_F1,  KC_F2,  KC_F3, KC_F4,   KC_F5,          KC_PMNS, KC_7,   KC_8, KC_9,  KC_EQL,
-      KC_F6,  CT_F7,  SH_F8, AL_F9,   GU_F10,         KC_PPLS, CT_4,   SH_5, AL_6,  GU_PDOT,
-      RESET, KC_PSCR, KC_NO, KC_F11,   KC_F12,        KC_0,    KC_1,   KC_2, KC_3,  KC_PCMM,
+      KC_PMNS, KC_7,   KC_8, KC_9,  KC_EQL   , KC_F1,  KC_F2,   KC_F3, KC_F4,   KC_F5,    
+      KC_PPLS, KC_4,   KC_5, KC_6,  KC_PDOT  , KC_F6,  KC_F7,   KC_F8, KC_F9,   KC_F10,
+      KC_0,    KC_1,   KC_2, KC_3,  KC_COMMA , RESET,  KC_PSCR, KC_NO, KC_F11,  KC_F12,
       KC_NO,   KC_NO,  KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_NO, KC_NO
                           ),
 
   [_MISC] = LAYOUT_absolem(
-      LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),    LGUI(KC_0), RGB_TOG, RGB_MOD,  RGB_VAI,    RGB_M_B,
-      LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), KC_VOLU,       KC_NLCK,     KC_LALT, KC_LSFT,  KC_LCTL,    KC_NO,
-      KC_MUTE,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_VOLD,       RGB_HUI,     RGB_SAI, RGB_VAI,  KC_NO,      KC_NO,
-      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,         KC_NO,       KC_NO,    KC_NO,    KC_NO,      KC_NO
+      LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),   LGUI(KC_0), RGB_TOG, RGB_MOD,  RGB_VAI, RGB_M_B,
+      LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), KC_VOLU,      KC_NLCK,    KC_LALT, KC_LSFT,  KC_LCTL, KC_MUTE,
+      RESET,      KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_VOLD,      RGB_HUI,    RGB_SAI, RGB_VAI,  KC_NO,   KC_NO,
+      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,        KC_NO,      KC_NO,   KC_NO,    KC_NO,   KC_NO
                            )
 
 };
